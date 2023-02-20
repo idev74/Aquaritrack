2aaf763fd7289a5e21987c5356493eda764f40c5
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-02-19 23:00:16 -0800

    partial setup

--- aquaritrack/main/routes.py
+++ aquaritrack/main/routes.py
@@ -1,5 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, Flask, render_template
 
 main = Blueprint('main', __name__)
 
-# Create your routes here.
+@main.route('/')
+def index():
+# Homepage - Displays the livestock and plants.
+    return render_template('base.html')
+
+
+# Login Page - The user enters info, and we authenticate the info.
+
+# Account Creation Page - The user creates their account and is led to the login page after.
+
+# Tank Creation Page - This allows users to add their items before submitting them.
+
+# Tank Detail Page - Displays info about livestock and plants and allows users to edit the description
+
+# Individual Item Page - Goes into further detail about each selected item.
--- aquaritrack/models.py
+++ aquaritrack/models.py
@@ -1,1 +1,48 @@
-# Create your models here.
+from extensions import db
+import enum
+
+class FormEnum(enum.Enum):
+    """Helper class to make it easier to use enums with forms."""
+    @classmethod
+    def choices(cls):
+        return [(choice.name, choice) for choice in cls]
+
+    def __str__(self):
+        return str(self.value)
+
+class LivestockType(FormEnum):
+    PLANT = 'Plant'
+    FISH = 'Fish'
+    INVERTEBRATE = 'Invertebrate'
+    AMPHIBIAN = 'Amphibian'
+    OTHER = 'other'
+
+#user model
+class User(db.Model):
+    """User Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+#tank model
+class Tank(db.Model):
+    """Tank Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    gallons = db.Column(db.Integer, nullable=False)
+    substrate = db.Column(db.String(80), nullable=False)
+    # created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # created_by = db.relationship('User')
+
+
+#item model
+class Item(db.Model):
+    """Tank Item Model"""
+    id = db.Column(db.Integer, primary_key=True)
+    species = db.Column(db.Float(precision=2), nullable=False)
+    # created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # created_by = db.relationship('User')
+
+ 
